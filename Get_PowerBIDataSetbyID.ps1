6be472ab5b69742bbeda0a0b6f6fc41b202d9577
Tim Macaulay <50430004+msfttimmac@users.noreply.github.com>
2019-06-21 18:35:04 -0400

    Get_PowerBIDataSetbyID
    Basic script that goes out and grabs the Power BI Data Set Name by just having the ID.

---
+++ Get_PowerBIDataSetbyID.ps1
@@ -0,0 +1,19 @@
+﻿#####################################################################################################
+# Get_PowerBIDataSetByID.ps1
+#
+# PURPOSE: 
+#    The purpose of this script is to get the Name of a Power BI DataSet when all we have is the DataSet ID.
+#
+# RESOURCES:
+#   Get-PowerBIDataSetID: https://docs.microsoft.com/en-us/powershell/module/microsoftpowerbimgmt.data/get-powerbidataset?view=powerbi-ps
+#   Connect-PowerBIServiceAccount: https://docs.microsoft.com/en-us/powershell/module/microsoftpowerbimgmt.profile/connect-powerbiserviceaccount?view=powerbi-ps
+#
+# CREATION DATE: June 21, 2019 - Tim Macaulay (timmac@microsoft.com)
+#####################################################################################################
+
+
+$PBIDataSetID = '<Data Set ID>'
+Connect-PowerBiServiceAccount
+$GetPBIDataSet = Get-PowerBIDataset -Id $PBIDataSetID -Scope Organization
+$GetPBIDataSetName = $GetPBIDataSet.Name
+$GetPBIDataSetName 

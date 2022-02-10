226bca9748e036627c9f73ff33cdcd98071ac0da
Tim Macaulay <50430004+msfttimmac@users.noreply.github.com>
2022-02-10 16:05:06 -0500

    Add files via upload

---
+++ WorkspaceScripting/PBIPS_WorkspaceUserInfo.ps1
@@ -0,0 +1,21 @@
+
+$UserObject = "admin@M365x229178.onmicrosoft.com"
+$pwdstring = "62Lu9iVEdB"
+$UserPass = ConvertTo-SecureString -String $pwdstring -AsPlainText -Force
+
+#$UserObect = ""
+#$pwdstring = ""
+#$UserPass = ConvertTo-SecureString -String $pwdstring -AsPlainText -Force
+$UserEnvironment = "Public"
+$UserCredential = New-Object -TypeName "System.Management.Automation.PSCredential" -ArgumentList $UserObject, $UserPass
+Connect-PowerBIServiceAccount -Credential $UserCredential -Environment $UserEnvironment
+(Invoke-PowerBIRestMethod -Url "https://api.powerbi.com/v1.0/myorg/admin/groups?%24expand=users&%24top=5000" -Method Get | ConvertFrom-Json).value | ForEach-Object{
+    $MyObject = New-Object PSObject
+    if ($_.Users.PrincipalType -eq "App") {
+        $MyObject | Add-Member -MemberType NoteProperty -Name "Workspace Name" -Value $_.Name -Force
+        $MyObject | Add-Member -MemberType NoteProperty -Name "Workspace Id" -Value $_.Id -Force
+        $MyObject | Add-Member -MemberType NoteProperty -Name "Workspace Users" -Value $_.Users -Force
+    }
+    
+    $MyObject
+}

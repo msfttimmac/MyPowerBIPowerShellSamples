4f10c33b2ea3f7276668418f6405dd80403e7538
Tim Macaulay <50430004+msfttimmac@users.noreply.github.com>
2022-10-13 11:03:20 -0400

    Add files via upload

---
+++ WorkspaceScripting/Find All Workspaces User Belongs.ps1
@@ -0,0 +1,14 @@
+﻿$EmailToCheck = "Iowatester5@pbigccsvcugi.onmicrosoft.com"
+
+Connect-PowerBIServiceAccount
+$url = "https://api.powerbi.com/v1.0/myorg/admin/groups?%24top=5000&%24expand=users"
+$GetAllPowerBIWorkspaces = (Invoke-PowerBIRestMethod -Url $url -Method Get -TimeoutSec 50000 | ConvertFrom-Json).value
+$GetAllPowerBIWorkspaces | ForEach-Object{
+    $CurrentObject = $_
+    $GetUsersEmailAddress = ($CurrentObject.users).emailAddress
+    if($GetUsersEmailAddress -eq $EmailToCheck){
+        $WorkSpaceObject = $CurrentObject.name + "( " + $CurrentObject.id + " )"
+        $WorkSpaceObject
+    }
+
+}

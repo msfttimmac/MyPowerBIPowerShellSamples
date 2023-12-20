f7242fe3aa9aef29ba4fc9469d73e936b7909395
Tim Macaulay <50430004+msfttimmac@users.noreply.github.com>
2023-12-20 17:30:11 -0500

    Update Get_WorkspaceAdmins.ps1

--- WorkspaceScripting/Get_WorkspaceAdmins.ps1
+++ WorkspaceScripting/Get_WorkspaceAdmins.ps1
@@ -1,4 +1,6 @@
 #Add your connection information here. 
+#$ExportLocation is going to be the folder location that you desire to export too. 
+
 #Power BI URL Information
 IF( $Environment -eq "USGov" ){
     $PowerBI_URL = "https://api.powerbigov.us/v1.0/myorg/admin"}
@@ -17,5 +19,5 @@ $Workspace_Information | ForEach-Object{
     $Current_Workspace_Object | Add-Member -MemberType NoteProperty -Name "Workspace Type" -Value $_.Type
     $Current_Workspace_Object | Add-Member -MemberType NoteProperty -Name "Workspace Admins" -Value ($_.Users | ConvertTo-Json) | Where-Object {$_.groupUserAccessRight -eq 'Admin'}
 
-    $Current_Workspace_Object | Export-CSV "$AutomationAccountFolder\WorkspaceAdmins.csv" -Append
+    $Current_Workspace_Object | Export-CSV "$ExportLocation\WorkspaceAdmins.csv" -Append
 }
